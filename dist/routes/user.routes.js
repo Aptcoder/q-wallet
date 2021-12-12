378c246d59b5614ff2fbaeafa3b98c702586cc80
Aptcoder <omilosamuel@gmail.com>
2021-12-12 21:54:01 +0100

    removed dist from index

--- dist/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = (0, express_1.Router)();
-router.get('/', function (req, res) { return res.send('users'); });
-exports.default = router;
